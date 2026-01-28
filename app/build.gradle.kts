25201026172f6a363ed9019743a458b9fef58ad9
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-01-29 00:55:56 +0800

    update kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     kotlin("plugin.serialization") version "2.0.21"
+    id("com.google.devtools.ksp")
 }
 
 android {
