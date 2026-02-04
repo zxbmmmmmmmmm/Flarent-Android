8e70b4d5d87531f1a69e86f3f63490445312489e
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-02-04 20:21:05 +0800

    fix: ime padding

--- app/src/main/java/com/bettafish/flarent/ui/pages/LoginPage.kt
+++ app/src/main/java/com/bettafish/flarent/ui/pages/LoginPage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -57,12 +58,12 @@ fun LoginPage(modifier: Modifier = Modifier,
                 navigationIcon = { BackNavigationIcon { navigator?.popBackStack()} })
         })
     { padding ->
-        val p = padding
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Center
+                .padding(padding)
+                .padding(24.dp)
+                .imePadding(),
         ) {
             Text(
                 text = "登录",
@@ -85,7 +86,7 @@ fun LoginPage(modifier: Modifier = Modifier,
                 onValueChange = { password = it },
                 label = { Text("密码") },
                 modifier = Modifier.fillMaxWidth(),
-                visualTransformation = PasswordVisualTransformation(), // 隐藏密码
+                visualTransformation = PasswordVisualTransformation(),
                 singleLine = true
             )
 
