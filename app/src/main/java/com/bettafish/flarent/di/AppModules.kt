22efd1f16767e4242a653479a0c68099be73517c
Betta_Fish <zxbmmmmmmmmm@outlook.com>
2026-05-25 17:42:33 +0800

    fix warnings

--- app/src/main/java/com/bettafish/flarent/di/AppModules.kt
+++ app/src/main/java/com/bettafish/flarent/di/AppModules.kt
@@ -52,7 +52,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
