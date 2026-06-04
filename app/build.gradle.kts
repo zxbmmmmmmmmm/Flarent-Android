33f95175a5e9f0a3d513edc24acd11e539f56cf4
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-06-04 17:46:20 +0800

    1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bettafish.flarent"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
